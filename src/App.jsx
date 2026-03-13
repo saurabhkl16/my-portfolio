391793466a0c7544c017fa2cba0cf937edc90007
saurabhKl16 <saurabhlom1998@gmail.com>
2026-03-13 10:03:03 +0530

    Added : Spinner component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,34 @@
-import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import Spinner from "./Components/spinner/spinner.jsx";
 import Navbar from "./Components/Navbar/Navbar.jsx";
-import Education from "./pages/education/Education.jsx";
-import Home from "./pages/home/Home.jsx";
-import Experience from "./pages/experience/Experience.jsx";
-import Projects from "./pages/projects/Projects.jsx";
-import OpenSource from "./pages/open source/OpenSource.jsx";
-import Contact from "./pages/contact/Contact.jsx";
+
+const Home = lazy(() => import("./pages/home/Home.jsx"));
+const Education = lazy(() => import("./pages/education/Education.jsx"));
+const Experience = lazy(() => import("./pages/experience/Experience.jsx"));
+const Projects = lazy(() => import("./pages/projects/Projects.jsx"));
+const Contact = lazy(() => import("./pages/contact/Contact.jsx"));
+
+// import OpenSource from "./pages/open source/OpenSource.jsx";
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
       <div className="routes">
-        <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/education" element={<Education />} />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/opensource" element={<OpenSource />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <Suspense fallback={<Spinner />}>
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/education" element={<Education />} />
+            <Route path="/experience" element={<Experience />} />
+            <Route path="/projects" element={<Projects />} />
+            {/* <Route path="/opensource" element={<OpenSource />} /> */}
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </Suspense>
       </div>
     </div>
   );
